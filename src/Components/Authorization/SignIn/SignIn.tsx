214c988f07e24e3981dadb73fff6086f30feb35f
Rinat B <rinatbuldybayev@gmail.com>
2023-03-29 16:22:05 +0600

    refactor SignIn component

--- src/Components/Authorization/SignIn/SignIn.tsx
+++ src/Components/Authorization/SignIn/SignIn.tsx
@@ -1,26 +1,36 @@
 import axios from 'axios'
 import s from './SignIn.module.css'
+import {useState} from "react";
 
 export const SignIn = () => {
-    const SendLoginToServer = () => {
-        axios.get('https://cards-nya-back-production.up.railway.app/')
-            .then(() => {
-                console.log('Hello')
-            })
+    const [email, setEmail] = useState('nya-admin@nya.nya');
+    const [password, setPassword] = useState('1qazxcvBG')
+    const [rememberMe, setRememberMe] = useState(false)
+
+    const data= {
+        email: email,
+        password: password,
+        rememberMe: false
     }
 
+    const SendLoginToServer = () => {
+        axios.post('https://cards-nya-back-production.up.railway.app/2.0/auth/login',data)
+            .then((response) => {
+                console.log(response.data)
+            })
+    }
 
     return (
         <div className={s.wrapper}>
             <header>Sign In</header>
             <div>
-                <input type="text"/>
+                <input type="text" value={email}/>
             </div>
             <div>
-                <input type="password"/>
+                <input type="password" value={password}/>
             </div>
             <div>
-                <input type="checkbox"/>
+                <input type="checkbox" checked={rememberMe}/>
             </div>
             <div>
                 Forgot Password?
